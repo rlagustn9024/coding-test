6639ff5f614c822ec80d92eb16c8eb718f197ba3
rlagustn9024 <gustn9024@naver.com>
2026-01-26 21:53:18 +0900

    백준 10828 풀이 완료

--- src/알고리즘_기초/자료구조/스택.java
+++ src/알고리즘_기초/자료구조/스택.java
@@ -1,9 +1,7 @@
 package 알고리즘_기초.자료구조;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class 스택 {
     public static void main(String[] args) throws IOException {
